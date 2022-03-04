290291d567f18ad79f34a84145359d270937a8f4
jrmkim50 <jrmkim50@gmail.com>
2022-03-03 20:22:57 -0800

    some logging

--- tfrecord.py
+++ tfrecord.py
@@ -146,7 +146,7 @@ def parse_example_fn(volume_shape, scalar_label=False):
         e = tf.io.parse_single_example(serialized=serialized, features=features)
         x = tf.io.decode_raw(e["feature/value"], _TFRECORDS_DTYPE)
         y = tf.io.decode_raw(e["label/value"], _TFRECORDS_DTYPE)
-        print(e["feature/shape"])
+        print(tf.io.decode_raw(e["feature/shape"], tf.int64))
         # TODO: this line does not work. The shape cannot be determined
         # dynamically... for now.
         # xshape = tf.cast(
